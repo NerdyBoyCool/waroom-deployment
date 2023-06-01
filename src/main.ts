0fbb9360ad69a4fa0e3b8e78815c73e7644b6440
NerdyBoyCool <takaya@nerdyboy.cool>
2023-06-01 16:12:13 +0900

    package action

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,48 @@
 import * as core from '@actions/core'
 import * as http from '@actions/http-client'
 
-const organization = core.getInput('organization', { required: true })
-const service = core.getInput('service', { required: true})
-const integrationToken = core.getInput('integration_token', { required: true })
-const version = core.getInput('version', { required: true })
-const environment = core.getInput('environment', { required: false })
-const description = core.getInput('description', { required: false })
-const repository_owner = core.getInput('repository_owner', { required: false })
-const repository_name = core.getInput('repository_name', { required: false })
+const organization = core.getInput('organization', {required: true})
+const service = core.getInput('service', {required: true})
+const integrationToken = core.getInput('integration_token', {required: true})
+const version = core.getInput('version', {required: true})
+const environment = core.getInput('environment', {required: false})
+const description = core.getInput('description', {required: false})
+const repository_owner = core.getInput('repository_owner', {required: false})
+const repository_name = core.getInput('repository_name', {required: false})
 const platform = 'github'
 
 type Deployment = {
-  ref: string;
-  environment?: string;
-  description?: string;
-  repository_owner?: string;
-  repository_name?: string;
-  platform: 'github';
+  ref: string
+  environment?: string
+  description?: string
+  repository_owner?: string
+  repository_name?: string
+  platform: 'github'
 }
 
 const createDeployment = async (): Promise<void> => {
   const client = new http.HttpClient()
-  const url = `http://localhost:3001/v1/organizations/${organization}/services/${service}/deployments`
+  const url = `http://api.beta.waroom.com/v1/organizations/${organization}/services/${service}/deployments`
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${integrationToken}`
-  };
+    Authorization: `Bearer ${integrationToken}`
+  }
 
   const body: Deployment = {
-    ref: version, repository_owner, repository_name, platform, environment, description
+    ref: version,
+    repository_owner,
+    repository_name,
+    platform,
+    environment,
+    description
   }
 
-  const response = await client.postJson(url, body, headers);
+  const response = await client.postJson(url, body, headers)
   const code = response.statusCode
-  if (code == 401) {
+  if (code === 401) {
     throw new Error('Deployment Integration Key is Invalid')
   }
-  if (code != 200) {
+  if (code !== 200) {
     throw new Error()
   }
 
