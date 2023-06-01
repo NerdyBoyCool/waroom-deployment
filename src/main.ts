3cf68d19ed3f3434a9f9b7309b6833692d249663
NerdyBoyCool <takaya@nerdyboy.cool>
2023-06-01 15:42:27 +0900

    refactor: error handling

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,23 @@
 import * as core from '@actions/core'
 import * as http from '@actions/http-client'
 
-// Q: Github Input から取得できるから、最初に取得しちゃっていいいよね？
 const organization = core.getInput('organization', { required: true })
 const service = core.getInput('service', { required: true})
-const integration_token = core.getInput('integration_token', { required: true })
+const integrationToken = core.getInput('integration_token', { required: true })
 const version = core.getInput('version', { required: true })
 const environment = core.getInput('environment', { required: false })
 const description = core.getInput('description', { required: false })
-const repository_owner = core.getInput('repository_owner', { required: true })
-const repository_name = core.getInput('repository_name', { required: true })
+const repository_owner = core.getInput('repository_owner', { required: false })
+const repository_name = core.getInput('repository_name', { required: false })
 const platform = 'github'
-
-type Service = {
-  name: string;
-}
 
 type Deployment = {
   ref: string;
-  environment: string;
-  description: string;
-  repository_owner: string;
-  repository_name: string;
+  environment?: string;
+  description?: string;
+  repository_owner?: string;
+  repository_name?: string;
   platform: 'github';
-  service: Service;
 }
 
 const createDeployment = async (): Promise<void> => {
@@ -31,26 +25,23 @@ const createDeployment = async (): Promise<void> => {
   const url = `http://localhost:3001/v1/organizations/${organization}/services/${service}/deployments`
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${integration_token}`
+    'Authorization': `Bearer ${integrationToken}`
   };
 
-  const body = {
-    ref: version, environment, description, repository_owner, repository_name, platform
+  const body: Deployment = {
+    ref: version, repository_owner, repository_name, platform, environment, description
   }
 
-  try {
-    const response = await client.postJson<Deployment>(url, body, headers);
-
-    // Q: エラーハンドリングしつつ、登録された情報をログに出力したい
-
-    // const result = response.result?.description
-    // const code = await response.result.
-    // console.log(code);
-  } catch(error) {
-    // Q: Retry 実装するべきだと思う？sentry-release 見ながら考えるか
-
-    console.error('HTTP request failed:', error);
+  const response = await client.postJson(url, body, headers);
+  const code = response.statusCode
+  if (code == 401) {
+    throw new Error('Deployment Integration Key is Invalid')
   }
+  if (code != 200) {
+    throw new Error()
+  }
+
+  core.info('Completion of Waroom Deployment Data Creation')
 }
 
 async function run(): Promise<void> {
